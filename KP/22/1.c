f5b1d9412f7f9a67c1363646ed2c8e15e9a91bac
adrian <mendienta990@gmail.com>
2023-11-05 21:24:43 +0700

    ..

---
+++ KP/22/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "strrev.h"
+
+int main() {
+    char str[100];
+
+    printf("Masukkan string: ");
+    fgets(str, sizeof(str), stdin);
+
+    strrev(str);
+
+    int len = strlen(str);
+
+    printf("Panjang string: %d\n", len);
+    printf("String terbalik: %s\n", str);
+
+    return 0;
+}
