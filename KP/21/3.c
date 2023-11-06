047e92d4f39377c5d4357923ff97b68d6a24f795
adrian <mendienta990@gmail.com>
2023-11-07 03:50:35 +0700

    .

--- KP/21/3.c
+++ KP/21/3.c
@@ -24,10 +24,6 @@ int pjgstr(char str[])
     int i = 0;
     while (str[i] != '\0')
     {
-        if (str[i] == '\n')
-        {
-            break;
-        }
         i++;
     }
     return i;
--- KP/22/1.c
+++ KP/22/1.c
@@ -3,14 +3,14 @@
 #include "strrev.h"
 
 int main() {
-    char str[100];
+    char str[100],copy[100];
 
     printf("Masukkan string: ");
     fgets(str, sizeof(str), stdin);
 
     strrev(str);
-
     int len = strlen(str);
+    
 
     printf("Panjang string: %d\n", len);
     printf("String terbalik: %s\n", str);
