a917e4c5c0a9a1e78ace5840cc8a8caf16d66e85
adrian <mendienta990@gmail.com>
2023-11-12 11:50:31 +0700

    Default

--- KP/24/1.c
+++ KP/24/1.c
@@ -4,10 +4,11 @@ int main()
 {
     int y, x = 87;
     int *px;
-    px = &x;
-    y = *px;
+    *px = 20;
+    
     printf("Alamat x = %p\n", &x);
     printf("Isi px = %p\n", px);
+    printf("nilai px = %d\n", *px);
     printf("Isi x = %d\n", x);
     printf("Nilai yang ditunjuk oleh px = %d\n", *px);
     printf("Nilai y = %d\n", y);
